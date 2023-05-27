dc83c866597d615230bb78689c9714940eb17714
xiao kaiwen <xiaokw@dragonpass.com.cn>
2023-05-27 08:30:22 +0800

    fix: logo image issue for production

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -21,7 +21,7 @@ export default function Home() {
           height="48"
           width="48"
           className="mx-auto w-auto"
-          src="/images/logo.png "
+          src="/images/logo.png"
         />
         <h2
           className={
